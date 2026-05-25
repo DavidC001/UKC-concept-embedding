45ca177c427f5b5b177afa3137c4a3608fa92ab0
davide <davidecavicchini@hotmail.com>
2026-05-25 16:20:33 +0200

    remove old train scripts for ukc_embedding

--- ukc_embedding/examples/test_RotE.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd .. 
-# source set_env.sh
-python test.py \
-            --model_dir hyp/logs/12_01/UKC_CUT_1_hyp_t/RotE_12_25_20/model_checkpoint/model_20251201_122520_best.pt 
-
-cd examples/
